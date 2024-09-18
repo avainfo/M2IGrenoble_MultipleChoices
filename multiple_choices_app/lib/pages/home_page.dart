5cc1fc07eccb27a952b987d0a045e6dd262c0eff
BuildTools <unconfigured@null.spigotmc.org>
2024-09-18 08:39:47 +0200

    Add title at the top of the page

--- multiple_choices_app/lib/pages/home_page.dart
+++ multiple_choices_app/lib/pages/home_page.dart
@@ -10,6 +10,41 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            
+            Container(
+              width: MediaQuery.of(context).size.width,
+              padding: const EdgeInsets.fromLTRB(0, 25, 0, 0),
+              child: RichText(
+                textAlign: TextAlign.center,
+                text: const TextSpan(
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  children: [
+                    TextSpan(
+                      text: "Get",
+                      style: TextStyle(
+                        color: Color(0xFF7322F5),
+                      ),
+                    ),
+                    TextSpan(
+                      text: "Surance",
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
