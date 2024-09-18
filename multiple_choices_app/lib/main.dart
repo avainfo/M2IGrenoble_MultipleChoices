8a85501cfec3e3d5ca8b87a138284e2454e217f0
BuildTools <unconfigured@null.spigotmc.org>
2024-09-18 08:36:39 +0200

    Add Home Page

--- multiple_choices_app/lib/main.dart
+++ multiple_choices_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:multiple_choices_app/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      home: HomePage(),
     );
   }
 }
